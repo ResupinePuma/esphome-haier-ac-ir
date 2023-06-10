2545adf79e398018426513cc2610335ebcde1ee9
ResupinePuma <cherchip50@gmail.com>
2023-06-10 13:25:12 +0300

    added vertical mode

--- components/haier_acyrw02/climate.py
+++ components/haier_acyrw02/climate.py
@@ -3,6 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import climate, sensor
 from esphome.const import CONF_ID, CONF_SENSOR_ID, CONF_PIN
 from esphome.core import CORE
+from esphome import automation
 
 AUTO_LOAD = ["climate"]
 
@@ -16,6 +17,14 @@ CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
         cv.Required(CONF_PIN): cv.int_
     }
 )
+CONF_VERTICAL_AIRFLOW = "vertical_airflow"
+VerticalAirflowAction = haier_acyrw02_ns.class_("VerticalAirflowAction", automation.Action)
+AirflowVerticalDirection = haier_acyrw02_ns.enum("AirflowVerticalDirection")
+AIRFLOW_VERTICAL_DIRECTION_OPTIONS = {
+    "UP": AirflowVerticalDirection.UP,
+    "CENTER": AirflowVerticalDirection.CENTER,
+    "DOWN": AirflowVerticalDirection.DOWN,
+}
 
 async def to_code(config):
     if CORE.is_esp8266 or CORE.is_esp32:
@@ -26,3 +35,25 @@ async def to_code(config):
     
     sens = await cg.get_variable(config[CONF_SENSOR_ID])
     cg.add(var.init(sens, config[CONF_PIN]))
+
+
+@automation.register_action(
+    "climate.haier.set_vertical_airflow",
+    VerticalAirflowAction,
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.use_id(HaierClimate),
+            cv.Required(CONF_VERTICAL_AIRFLOW): cv.templatable(
+                cv.enum(AIRFLOW_VERTICAL_DIRECTION_OPTIONS, upper=True)
+            ),
+        }
+    ),
+)
+async def haier_set_vertical_airflow_to_code(config, action_id, template_arg, args):
+    paren = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg, paren)
+    template_ = await cg.templatable(
+        config[CONF_VERTICAL_AIRFLOW], args, AirflowVerticalDirection
+    )
+    cg.add(var.set_direction(template_))
+    return var
--- components/haier_acyrw02/haier_acyrw02.h
+++ components/haier_acyrw02/haier_acyrw02.h
@@ -12,6 +12,24 @@
 namespace esphome {
 namespace haier_acyrw02 {
 
+enum VerticalSwingMode : uint8_t {
+  MAX_UP = kHaierAc160SwingVHighest,
+  UP = kHaierAc160SwingVTop,
+  CENTER = kHaierAc160SwingVMiddle,
+  DOWN = kHaierAc160SwingVLow,
+  MAX_DOWN = kHaierAc160SwingVLowest,
+  AUTO = kHaierAc160SwingVAuto
+};
+
+enum class AirflowVerticalDirection : uint8_t {
+  MAX_UP = 0,
+  UP = 1,
+  CENTER = 2,
+  DOWN = 3,
+  MAX_DOWN = 4,
+  AUTO = 5
+};
+
 const uint8_t HAIER_ACYRW02_TEMP_MIN = 16;  // 16C
 const uint8_t HAIER_ACYRW02_TEMP_MAX = 30;  // 32C
 
@@ -19,13 +37,18 @@ class HaierClimate : public climate::Climate {
  public:
   HaierClimate() : climate::Climate() {}
  public:
+  void set_vertical_airflow(AirflowVerticalDirection direction);
+  
   void set_sensor(sensor::Sensor *sensor);
   void init(sensor::Sensor *sensor, uint16_t pin);
  
  protected:
   IRHaierACYRW02 *ac_{nullptr};
   sensor::Sensor *sensor_{nullptr};
-  
+
+  VerticalSwingMode get_vertical_swing_mode(AirflowVerticalDirection direction);
+
+  uint8_t vertical_direction_;  
   void setup_ir_cmd();
 
   climate::ClimateTraits traits() override;
